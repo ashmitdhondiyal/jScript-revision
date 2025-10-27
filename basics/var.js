2ff6be7fb28d628a6d0fb39a5f518d2ff78350f5
ashmitdhondiyal <ashmitdhondiyal2005@gmail.com>
2025-10-27 09:14:49 +0000

    variables in js

---
+++ basics/var.js
@@ -0,0 +1,17 @@
+const accountId = 2005; //i cannot modify it later
+let Name = "ashmit"; 
+var state = "delhi";
+country = "india"; //we should not use this method
+let number; //this will print UNDEFINED
+
+console.log([accountId, Name, state, country])
+
+//accountId=3; //can't do this
+/* we will not use var,
+instead we will use let*/
+
+Name = "dhondiyal";
+state = "uttarakhand";
+country = "bhaarat";
+
+console.table([Name, state, country])
