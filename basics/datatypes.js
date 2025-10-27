0b113726f96cf57f904727b6032dea2e739ebcd4
ashmitdhondiyal <ashmitdhondiyal2005@gmail.com>
2025-10-27 13:39:33 +0000

    datatypes in js

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //strict mode of javascript
+
+//alert(2+3); //alert only works on browser not on ide
+
+console.log(typeof 3); //number
+console.log(typeof "ashmit"); //string
+console.log(typeof ashmit); //undefined(cuz we haven't defined its datatype)
+
+let name = "ashmit"; //string
+let age = 20; //age
+let passport = false; //boolean
+let married = null; //typeof null is an object
+let undefined; //typeof undefined is undefined
+
+//undefined and null is different
+
+
