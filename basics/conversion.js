153cd4b6cbf8e838ce15f7a78a53405fe7d01f3a
ashmitdhondiyal <ashmitdhondiyal2005@gmail.com>
2025-11-05 11:11:32 +0000

    coversion of dataTypes

---
+++ basics/conversion.js
@@ -0,0 +1,36 @@
+let abc = "123" //this is string
+//console.log(typeof abc);
+//
+let xyz = Number(abc) //string has been converted to number
+//console.log(typeof xyz);
+//console.log(xyz);
+
+/*conversions in number!!!
+"123" => 123
+"123a" => NaN(not a number)
+true => 1
+false => 0
+*/
+
+let logginId=1
+
+let boolLogginId=Boolean(logginId)
+//console.log(boolLogginId);
+
+/*Conversion in boolean
+1 => true
+0 => false
+"" => false
+"ashmit" => true
+*/
+
+let num = 1;
+//console.log(num * 3); //3
+
+
+let stringNum = String(num)
+//console.log(stringNum * 3); //3
+//console.log(typeof stringNum); //string
+
+let a = "abc"
+console.log(a * 5); //NaN
