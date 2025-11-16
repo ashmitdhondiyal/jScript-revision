bb82d9e43215c8135284485271513d6bd72a285c
ashmitdhondiyal <ashmitdhondiyal2005@gmail.com>
2025-11-16 19:16:56 +0000

    non primitive learned

---
+++ basics/nonprimitive.js
@@ -0,0 +1,21 @@
+//Array, object, function
+
+//1.Array:
+const heroes = ["ironman", "hulk", "captain-america"];
+//console.log(typeof heroes); //object
+
+
+//2.Object:
+let myObj = {
+    name:"ashmit dhondiyal",
+    age:20,
+};
+//console.log(myObj.name); //ashmit dhondiyal
+//console.log(typeof myObj); //Object
+
+//3.Function:
+const myFunc = function(){
+    console.log("hello world!");
+    
+}
+//console.log(typeof myFunc); //function
